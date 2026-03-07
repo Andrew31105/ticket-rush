d8f61a513b411f1cd7a5176cd6fad724a1edbbe7
Andrew31105 <daoanh3112005ae@gmail.com>
2026-03-07 11:44:18 +0700

    jwt

--- src/main/java/com/ticketrush/config/SecurityConfig.java
+++ src/main/java/com/ticketrush/config/SecurityConfig.java
@@ -1,4 +1,16 @@
 package com.ticketrush.config;
 
+
+import com.ticketrush.filter.JWTFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JWTFilter jwtFilter;
+    private final
 }
---
+++ src/main/java/com/ticketrush/service/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.ticketrush.service;
+
+import com.ticketrush.entity.User;
+import com.ticketrush.repository.IUserRepository;
+import com.ticketrush.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.nio.file.attribute.UserPrincipal;
+
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final IUserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        return new UserPrincipal(user);
+
+    }
+}
